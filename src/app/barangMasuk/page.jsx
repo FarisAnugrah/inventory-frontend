8d0a4931b8e5a592ab04b5bf267db3058428dd7d
ermaardelia <111202315356@mhs.dinus.ac.id>
2025-04-30 12:53:24 +0700

    add tanggal masuk

--- src/app/barangMasuk/page.jsx
+++ src/app/barangMasuk/page.jsx
@@ -22,10 +22,10 @@ export default function barangMasuk() {
   ];
 
   const data = [
-    { no: 1, user: "Suci Indah", kode: "BRG-1234567895", nama: "Indomie", supplier: "CV Indah", stok: "100", satuan: "Karton" },
-    { no: 2, user: "Suci Indah", kode: "BRG-1234567897", nama: "Le Minerale", supplier: "CV Intan", stok: "20", satuan: "Karton" },
-    { no: 3, user: "Suci Indah", kode: "BRG-1234567898", nama: "Tolak Angin", supplier: "CV Persada", stok: "3", satuan: "Karton" },
-    { no: 4, user: "Suci Indah", kode: "BRG-1234567893", nama: "Lampu Phillips", supplier: "CV Utama", stok: "40", satuan: "Karton" },
+    { no: 1, user: "Suci Indah", kode: "BRG-1234567895", nama: "Indomie", supplier: "CV Indah", stok: "100", satuan: "Karton" , tanggal: "20-05-2025" },
+    { no: 2, user: "Suci Indah", kode: "BRG-1234567897", nama: "Le Minerale", supplier: "CV Intan", stok: "20", satuan: "Karton" , tanggal: "15-05-2025" },
+    { no: 3, user: "Suci Indah", kode: "BRG-1234567898", nama: "Tolak Angin", supplier: "CV Persada", stok: "3", satuan: "Karton", tanggal: "16-05-2025" },
+    { no: 4, user: "Suci Indah", kode: "BRG-1234567893", nama: "Lampu Phillips", supplier: "CV Utama", stok: "40", satuan: "Karton" , tanggal: "17-05-2025"},
 
   ];
   return (
